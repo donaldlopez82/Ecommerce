882923246a0a87a10ac2b7db64002a497c95a91b
donaldlopez82 <donaldlopez@donaldlopezart.com>
2022-06-07 19:45:33 -0700

    Created-backend-server

--- comicbuy/README.md
+++ comicbuy/README.md
@@ -6,3 +6,5 @@
 2. Create React App
 3. Create Git Repository
 4. Created feature products
+5. Create Routes and Components
+6. Node js Server
---
+++ comicbuy/backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Batman Comic',
+      slug: 'batman-comic',
+      category: 'comics',
+      image: '/images/batmanwholaughs.jpg',
+      price: 12,
+      countInStock: 10,
+      company: 'DC',
+      rating: 4.5,
+      numReviews: 110,
+      description: 'Batman vs Joker',
+    },
+    {
+      name: 'Spider-Man Comic',
+      slug: 'spider-man-comic',
+      category: 'comics',
+      image: '/images/spiderman.jpg',
+      price: 12,
+      countInStock: 10,
+      company: 'Marvel',
+      rating: 4.7,
+      numReviews: 150,
+      description: 'Spider-Man Spiders',
+    },
+    {
+      name: 'Avengers Comic',
+      slug: 'avengers-comic',
+      category: 'comics',
+      image: '/images/avengers.jpg',
+      price: 12,
+      countInStock: 10,
+      company: 'Marvel',
+      rating: 4.0,
+      numReviews: 99,
+      description: 'Avengers Origin',
+    },
+    {
+      name: 'Justice League Comic',
+      slug: 'justice-league-comic',
+      category: 'comics',
+      image: '/images/justiceleague.jpg', //679px x 829px
+      price: 12,
+      countInStock: 10,
+      company: 'DC',
+      rating: 3.5,
+      numReviews: 111,
+      description: 'Justice League Origin',
+    },
+  ],
+};
+export default data;
